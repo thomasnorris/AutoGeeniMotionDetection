ebd19760d89def49c697eabf9d18169af427a5ba
Thomas Norris <tom.norris135@gmail.com>
2019-11-19 17:47:02 -0500

    import

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# AutoGeeniMotionDetection
-Enable/disable motion detection on Geeni cameras based on who is home
+## Prerequisites
+### NPM and Node.js
+- Both of these are required and can be downloaded [here](https://github.com/nodesource/distributions).
+    - If using a Raspberry Pi Zero W, follow [these](https://www.thepolyglotdeveloper.com/2018/03/install-nodejs-raspberry-pi-zero-w-nodesource/) instructions instead.
+### Geeni
+- Geeni is connected to the Google Assistant.
+- The cameras are named the same as in `program.js`.
+    - This can be done through the Google Home app or the Geeni app.
+### Google Assistant Server
+- [REST-GoogleAssistant]() must be set up and operational.
+    - Take note of the port that the server is listening on and make sure that the port is forwarded.
+## Installation
+- Everything in the __Prerequisites__ section must be done first!
+- After cloning:
+    - Run `npm install` to install packages.
+    - Run `node program.js` to start.
+- Optionally, add the following line to `/etc/rc.local` for auto startup:
+    - `sudo node "/PATH/TO/REPO/program.js"`.
---
+++ program.js
@@ -0,0 +1,51 @@
+(function() {
+    const CAM_1 = 'Doggo Cam';
+    const CAM_2 = 'People (Ellie) Cam';
+    const ENABLE_MD = 'Enable motion detection';
+    const DISABLE_MD = 'Disable motion detection';
+    // MACs should be cupper case
+    const MACS = ['CC:C0:79:F1:8F:47', 'CC:C0:79:83:5B:18'];
+    const SCAN_INTERVALS = {
+        HOME: '5000',
+        AWAY: '2000'
+    };
+
+    var _path = require('path');
+    var _scan = require('local-devices');
+
+    var _scanIntervalMs = SCAN_INTERVALS.AWAY;
+    var _away = true;
+
+    (function startScanning(init = false) {
+        _away ? console.log('Away') : console.log('Home');
+
+        _scan().then((devices) => {
+            console.log('Scanned', devices.length, 'devices.');
+            var match = devices.some((device) => {
+                return MACS.includes(device.mac.toUpperCase());
+            });
+
+            if (match && _away) {
+                // someone just came home
+                sendCommand(DISABLE_MD + ' on ' + CAM_1);
+                sendCommand(DISABLE_MD + ' on ' + CAM_2);
+                _scanIntervalMs = SCAN_INTERVALS.HOME;
+                _away = false;
+            } else if (init || (!match && !_away)) {
+                // everyone just left
+                sendCommand(ENABLE_MD + ' on ' + CAM_1);
+                sendCommand(ENABLE_MD + ' on ' + CAM_2);
+                _scanIntervalMs = SCAN_INTERVALS.AWAY;
+                _away = true;
+            }
+
+            setTimeout(() => {
+                startScanning();
+            }, _scanIntervalMs);
+        });
+    })(true);
+
+    function sendCommand(command) {
+
+    }
+})();
