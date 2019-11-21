edb254d5222e73dcce1adad05b494509d7814763
Thomas Norris <tom.norris135@gmail.com>
2019-11-20 19:59:05 -0500

    deleted old code by mistake

--- program.js
+++ program.js
@@ -8,7 +8,8 @@
     const ENABLE_MD = 'Enable motion detection';
     const DISABLE_MD = 'Disable motion detection';
 
-    // Any new IPS must have a manual 'await ping(ip)' function call inside while loop
+    // IPs must be static
+    // Any new IPS must have a manual 'await ping(ip)' function call
     const IPS = {
         TOM: '192.168.0.25'
     }
@@ -16,6 +17,8 @@
     const PING_CFG = {
         timeout: 2
     }
+
+    // todo: change to ping timeout
     const SCAN_INTERVALS = {
         HOME: '5000',
         AWAY: '2000'
@@ -33,11 +36,45 @@
         while (true) {
             // need to manually ping devices
             var match = await ping(IPS.TOM);
-
             var res = await sendCommand('how is the weather');
             console.log(res);
         }
     })();
+
+    // (function run(init = false) {
+    //     var match;
+    //     IPS.forEach((ip) => {
+    //         _ping.sys.probe(ip, (alive) => {
+    //             match = alive;
+    //         }, {
+    //             timeout: 2
+    //         });
+    //     });
+
+    //     setTimeout(() => {
+    //         console.log('Match:', match);
+    //         console.log('Status:', _away ? 'Away' : 'Home');
+    //         if (match && _away) {
+    //             // someone just came home
+    //             sendCommand('What time is it?', (res) => {
+    //                 _scanIntervalMs = SCAN_INTERVALS.HOME;
+    //                 _away = false;
+    //             });
+    //             //sendCommand(DISABLE_MD + ' on ' + CAM_1);
+    //             //sendCommand(DISABLE_MD + ' on ' + CAM_2);
+    //         } else if (init || (!match && !_away)) {
+    //             // everyone just left
+    //             sendCommand('How is the weather today?', (res) => {
+    //                 _scanIntervalMs = SCAN_INTERVALS.AWAY;
+    //                 _away = true;
+    //             });
+    //             //sendCommand(ENABLE_MD + ' on ' + CAM_1);
+    //             //sendCommand(ENABLE_MD + ' on ' + CAM_2);
+    //         }
+    //         run();
+    //     }, _scanIntervalMs);
+
+    // })(true);
 
     function sendCommand(command) {
         return new Promise((resolve, reject) => {
