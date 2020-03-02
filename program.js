5178f7992fa4ba35ea5012f6360a38fbd78636a0
Thomas Norris <tom.norris135@gmail.com>
2020-03-02 18:06:16 +0000

    added logging to the assistant client; change logger in program.js

--- program.js
+++ program.js
@@ -1,120 +1,95 @@
 var _path = require('path');
 var _logger = require(_path.resolve(__dirname, 'Node-Logger', 'app.js'));
-(async function() {
-    await _logger.Init();
-    main();
-})();
+_logger.Init();
 
-function main() {
-    var _ping = require('ping');
-    var _assistant = require(_path.resolve(__dirname, 'REST-GoogleAssistant-Client', 'client.js'));
+var _ping = require('ping');
+var _assistant = require(_path.resolve(__dirname, 'REST-GoogleAssistant-Client', 'client.js'));
 
-    const CAM_1 = 'Living Room Cam';
-    const CAM_2 = 'Doggo Cam';
-    const ENABLE_MD = 'Enable motion detection';
-    const DISABLE_MD = 'Disable motion detection';
+const CAM_1 = 'Living Room Cam';
+const CAM_2 = 'Doggo Cam';
+const ENABLE_MD = 'Enable motion detection';
+const DISABLE_MD = 'Disable motion detection';
 
-    const SCAN_WAIT_MS = 2000;
-    const PING_TIMEOUT_S = 2;
+const SCAN_WAIT_MS = 2000;
+const PING_TIMEOUT_S = 2;
 
-    // IPs must be static, set at router level
-    const IPS = {
-        TOM: '192.168.0.25',
-        NATH: '192.168.0.26'
-    }
+// IPs must be static, set at router level
+const IPS = {
+    TOM: '192.168.0.25',
+    NATH: '192.168.0.26'
+}
 
-    var _away;
-    // main
-    (async function(init) {
-        while (true) {
-            // check for a device match
-            var match = await pingAll();
-            var status = init ? 'Init' : _away ? 'Away' : 'Home'
+var _away;
+// main
+(async function(init) {
+    while (true) {
+        // check for a device match
+        var match = await pingAll();
+        var status = init ? 'Init' : _away ? 'Away' : 'Home'
 
-            console.log('Status:', status);
+        console.log('Status:', status);
 
-            // someone just came home
-            if (match && _away) {
-                _logger.Info.Async('Status change', 'Someone came home');
-                await _assistant.Send(DISABLE_MD + ' on ' + CAM_1)
-                    .then((response) => {
-                    })
-                    .catch((err) => {
-                        _logger.Error.Async('Command(s) not sent', err);
-                    });
-                await _assistant.Send(DISABLE_MD + ' on ' + CAM_2)
-                    .then((response) => {
-                    })
-                    .catch((err) => {
-                        _logger.Error.Async('Command(s) not sent', err);
-                    });
-                _away = false;
-            }
-
-            // everyone is away or init
-            else if (init || (!match && !_away)) {
-                if (init)
-                    init = false;
-
-                _logger.Info.Async('Status change', 'Everyone just left');
-                await _assistant.Send(ENABLE_MD + ' on ' + CAM_1)
-                    .then((response) => {
-                    })
-                    .catch((err) => {
-                        _logger.Error.Async('Command(s) not sent', err);
-                    });
-                await _assistant.Send(ENABLE_MD + ' on ' + CAM_2)
-                    .then((response) => {
-                    })
-                    .catch((err) => {
-                        _logger.Error.Async('Command(s) not sent', err);
-                    });
-                _away = true;
-            }
-
-            // only have to wait if someone is home because scanning all devices when
-            // no one is home introduces a delay
-            if (!_away)
-                await wait(SCAN_WAIT_MS);
-        }
-    })(true);
-
-    function wait(ms) {
-        return new Promise((resolve, reject) => {
-            setTimeout(resolve, ms);
-        });
-    }
-
-    function pingAll() {
-        var pingCfg = {
-            timeout: PING_TIMEOUT_S
+        // someone just came home
+        if (match && _away) {
+            _logger.Info.Async('Status change', 'Someone came home');
+            await _assistant.Send(DISABLE_MD + ' on ' + CAM_1);
+            await _assistant.Send(DISABLE_MD + ' on ' + CAM_2);
+            _away = false;
         }
 
-        // build promises to ping each IP
-        var promiseArr = Object.keys(IPS).map((i) => {
-            return new Promise((resolve, reject) => {
-                var ip = IPS[i];
-                // ping the ip and resolve
-                _ping.sys.probe(ip, (alive) => {
-                    // if alive, reject to prevent pinging other devices
-                    if (alive)
-                        reject();
+        // everyone is away or init
+        else if (init || (!match && !_away)) {
+            if (init)
+                init = false;
 
-                    // no match, resolve false and ping next device
-                    resolve();
-                }, pingCfg);
-            });
-        });
+            _logger.Info.Async('Status change', 'Everyone just left');
+            await _assistant.Send(ENABLE_MD + ' on ' + CAM_1);
+            await _assistant.Send(ENABLE_MD + ' on ' + CAM_2);
+            _away = true;
+        }
 
-        return new Promise((resolve, reject) => {
-            // wait for all ping promises to resolve
-            Promise.all(promiseArr).then(() =>{
-                // no device was found
-                resolve(false);
-            }).catch(() => {
-                // a device was found, so the rest were skipped
-                resolve(true);
-            });
-        })
+        // only have to wait if someone is home because scanning all devices when
+        // no one is home introduces a delay
+        if (!_away)
+            await wait(SCAN_WAIT_MS);
     }
-};
+})(true);
+
+function wait(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+function pingAll() {
+    var pingCfg = {
+        timeout: PING_TIMEOUT_S
+    }
+
+    // build promises to ping each IP
+    var promiseArr = Object.keys(IPS).map((i) => {
+        return new Promise((resolve, reject) => {
+            var ip = IPS[i];
+            // ping the ip and resolve
+            _ping.sys.probe(ip, (alive) => {
+                // if alive, reject to prevent pinging other devices
+                if (alive)
+                    reject();
+
+                // no match, resolve false and ping next device
+                resolve();
+            }, pingCfg);
+        });
+    });
+
+    return new Promise((resolve, reject) => {
+        // wait for all ping promises to resolve
+        Promise.all(promiseArr).then(() =>{
+            // no device was found
+            resolve(false);
+        }).catch(() => {
+            // a device was found, so the rest were skipped
+            resolve(true);
+        });
+    })
+}
