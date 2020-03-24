15256e1c6f944d950ade56215b8b9a21e5e50ff7
Thomas Norris <tom.norris135@gmail.com>
2020-03-24 08:33:18 -0400

    Away Delay (#6)
    * started to introduce a delay
    
    * changed .gitignore; removed local-modules.js; seems to work?
    
    * reverted comments; testing
    
    * update submodules; changed logger import location to be last of the submodules
    
    * changed logger debug back to info
    
    * Update README.md

--- program.js
+++ program.js
@@ -1,9 +1,8 @@
 
 var _path = require('path');
-var _logger = require(_path.resolve(__dirname, 'Node-Logger', 'app.js'));
-
 var _ping = require('ping');
 var _assistant = require(_path.resolve(__dirname, 'REST-GoogleAssistant-Client', 'client.js'));
+var _logger = require(_path.resolve(__dirname, 'Node-Logger', 'app.js'));
 
 const CAM_1 = 'Living Room Cam';
 const CAM_2 = 'Doggo Cam';
@@ -13,6 +12,10 @@ const DISABLE_MD = 'Disable motion detection';
 const SCAN_WAIT_MS = 2000;
 const PING_TIMEOUT_S = 2;
 
+// cycles to wait before sending commands
+// prevents commands from being fired if someone disconnects temporarily
+const DELAY_CYCLES = 5;
+
 // IPs must be static, set at router level
 const IPS = {
     TOM: '192.168.0.25',
@@ -20,14 +23,17 @@ const IPS = {
 }
 
 var _away;
+var _wait_cycle_count = 0;
+
 // main
-(async function(init) {
+(async (init) => {
     while (true) {
         // check for a device match
         var match = await pingAll();
-        var status = init ? 'Init' : _away ? 'Away' : 'Home'
 
-        console.log('Status:', status);
+        // reset the cycle count and prevent command from being sent
+        if ((match && !_away) || !match && _away)
+            _wait_cycle_count = 0;
 
         // someone just came home
         if (match && _away) {
@@ -35,17 +41,22 @@ var _away;
             await _assistant.Send(DISABLE_MD + ' on ' + CAM_1);
             await _assistant.Send(DISABLE_MD + ' on ' + CAM_2);
             _away = false;
+            _wait_cycle_count = 0;
         }
 
         // everyone is away or init
         else if (init || (!match && !_away)) {
-            if (init)
-                init = false;
+            if (_wait_cycle_count === DELAY_CYCLES || init) {
+                if (init)
+                    init = false;
 
-            _logger.Info.Async('Status change', 'Away');
-            await _assistant.Send(ENABLE_MD + ' on ' + CAM_1);
-            await _assistant.Send(ENABLE_MD + ' on ' + CAM_2);
-            _away = true;
+                _logger.Info.Async('Status change', 'Away');
+                await _assistant.Send(ENABLE_MD + ' on ' + CAM_1);
+                await _assistant.Send(ENABLE_MD + ' on ' + CAM_2);
+                _away = true;
+                _wait_cycle_count = 0;
+            } else
+                ++_wait_cycle_count;
         }
 
         // only have to wait if someone is home because scanning all devices when
